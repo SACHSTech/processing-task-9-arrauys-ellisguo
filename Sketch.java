5f62b118ab1db9c0da6d82c81f316fda46647889
Ellis Guo <99352765+ellisguo@users.noreply.github.com>
2022-05-16 02:22:25 +0000

    homework

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,137 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    // put your size call here
+    size(300, 300);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  float[] snowflakeY = new float[35];
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
+
+  float playerX = 200;
+  float playerY = 200; 
+
+  float[] clicked = new float[5];
+
+  float clickedX;
+  float clickedY;
+  float newclick;
+
+  int count = 0;
+
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < snowflakeY.length; i++) {
+      snowflakeY[i] = random(height);
+    }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    background(50);
+
+    float z = 2;
+
+    for (int i = 0; i < snowflakeY.length; i++) {
+
+      float snowflakeX = width * i / snowflakeY.length;
+      
+      fill(225);
+      ellipse(snowflakeX, snowflakeY[i], 10, 10);
+      
+      if (snowflakeX - clickedX < 2 && snowflakeY[i] - clickedY < 5) {
+        snowflakeY[i] = newclick;
+        fill(50);
+        ellipse(clickedX, newclick, 15, 15);
+      }
+      
+      snowflakeY[i] += z;
+      newclick += z;
+
+      if (snowflakeY[i] > height) {
+        snowflakeY[i] = 0;
+      }
+
+      if (keyPressed) {
+        if (keyCode == DOWN) {
+          z = 3;
+        }
+        if (keyCode == UP) {
+          z = 1;
+        }
+      }
+
+      if (upPressed) {
+        playerY--;
+      }
+      if (downPressed) {
+        playerY++;
+      }
+      if (leftPressed) {
+        playerX--;
+      }
+      if (rightPressed) {
+        playerX++;
+      }
+
+      fill(0, 0, 255);
+      ellipse(playerX, playerY, 20, 20);
+
+      if (dist(playerX, playerY, snowflakeX, snowflakeY[i]) < 3 && dist(playerX, playerY, snowflakeX, snowflakeY[i]) > 2) {
+        count++;
+      }
+
+      
+    
+    }
+
+    if (count >= 3) {
+      background(255);
+      exit();
+    } 
+
+    for (int g = count; g <= 3; g++){
+      rect(((g)*100), 50, 25, 25);
+    }
+
+    System.out.println(count);
+
   }
-  
-  // define other methods down here.
+
+  public void mouseClicked() {
+    clickedX = mouseX;
+    clickedY = mouseY;
+  }
+
+  public void keyPressed() {
+
+    if (key == 'w') {
+      upPressed = true;
+    } else if (key == 's') {
+      downPressed = true;
+    } else if (key == 'a') {
+      leftPressed = true;
+    } else if (key == 'd') {
+      rightPressed = true;
+    }
+
+  }
+
+  public void keyReleased() {
+
+    if (key == 'w') {
+      upPressed = false;
+    } else if (key == 's') {
+      downPressed = false;
+    } else if (key == 'a') {
+      leftPressed = false;
+    } else if (key == 'd') {
+      rightPressed = false;
+    }
+  }
+
 }
